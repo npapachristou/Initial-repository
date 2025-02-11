5c9b198d786db4df01c0604b4535d26896d73032
npapachristou <nikospapachristou014@gmail.com>
2025-02-11 02:09:49 +0200

    First UBUNTU Commit

--- hellogo/main.go
+++ hellogo/main.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello from go Experiment created branch,  it's perfect there !!!")
+	fmt.Println("Hello from go Experiment created branch in Ubuntu,  it's perfect there !!!")
 }
